8f4104b62634b5da309fef7001128fd3ba00e566
Eddie Huang <edhuang04@gmail.com>
2020-10-31 18:19:48 -0800

    added the movement marker ( IE the rat)

--- assets/tile.js
+++ assets/tile.js
@@ -1,5 +1,5 @@
 var SIZE = 25;
-var TYPES = ["BARRIER", "OPEN", "BISCUIT", "CHERRY"];
+var TYPES = ["BARRIER", "OPEN", "BISCUIT", "CHERRY", "GHOST", "PACMAN"];
 
 var oneHalf = SIZE / 2;
 var oneThird = SIZE / 3;
@@ -11,8 +11,27 @@ function Tile(x,y,type) {
     this.y = y;
     this.type = type;  //barrier
     
+    this.dX = -1;
+    this.dY = -1;
+    this.moving = false;
+
+    this.speed = 0.2;
 
 }
+
+Tile.prototype.update = function() 
+{
+    if (this.moving)
+    {
+        this.x = lerp(this.x, this.dX, this.speed);
+        this.y = lerp(this.y, this.dY, this.speed);
+        
+        if (this.x == this.dX && this.y == this.dY)
+            this.moving = false;
+    }
+};
+
+
 
 Tile.prototype.draw = function() 
 {
@@ -43,7 +62,14 @@ Tile.prototype.draw = function()
             fill("#FF2222");
             ellipse(this.x * SIZE + oneForth, this.y * SIZE + oneForth, oneHalf);
             break;
-
+        case "GHOST":
+            break;
+        case "PACMAN":
+            ellipseMode(CORNER);
+            stroke("#FFFF22");
+            strokeWeight(10);
+            ellipse(this.x * SIZE + oneThird, this.y * SIZE + oneThird, oneThird);
+            break;
     }
 };
 
@@ -59,5 +85,22 @@ function parseTileType(t)
             return "BISCUIT";
         case "3":
             return "CHERRY";
+        case "4":
+            return "GHOST";
+        case "5":
+            return "PACMAN";
     }
+}
+
+Tile.prototype.move = function(x, y) 
+{
+    var dY = this.y + y;
+    var dX = this.x + x;
+
+    if(this.moving)
+        return;
+
+    var destinationTile = field[dY * DIMENSIONS + dX];
+    var type = destinationTile.type;
+
 }
